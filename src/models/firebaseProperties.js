729891e4ed47bd19cc5e6bfd25484e1c84ccaf45
Jagmoah Singh <singhjagmohan4734@gmail.com>
2020-04-13 08:59:10 -0700

    Making some changes

--- src/models/firebaseProperties.js
+++ src/models/firebaseProperties.js
@@ -4,7 +4,7 @@ const CONFIG = require(path.join(HOMEDIR, "config", "default"));
 const admin = require('firebase-admin');
 admin.initializeApp({
     credential: admin.credential.cert({
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
         "project_id": process.env.FIREBASE_PROJECT_ID
     }),
