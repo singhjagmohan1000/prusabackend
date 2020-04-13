75ec60ca2f67c7583135087cb9467d26a407c410
Jagmoah Singh <singhjagmohan4734@gmail.com>
2020-04-13 09:15:16 -0700

    Making some changes

--- src/models/firebaseProperties.js
+++ src/models/firebaseProperties.js
@@ -3,11 +3,7 @@ const HOMEDIR = path.join(__dirname, "..", "..");
 const CONFIG = require(path.join(HOMEDIR, "config", "default"));
 const admin = require('firebase-admin');
 admin.initializeApp({
-    credential: admin.credential.cert({
-        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-        "project_id": process.env.FIREBASE_PROJECT_ID
-    }),
+    credential: admin.credential.applicationDefault(),
     databaseURL: CONFIG.FIREBASE.DATABASE_URL
 });
 const fbData = admin.firestore();
