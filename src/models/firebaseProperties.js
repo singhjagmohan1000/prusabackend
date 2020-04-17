70b2137114dbfeeb3c5d176c0483e481be3dfd2d
Jagmoah Singh <singhjagmohan4734@gmail.com>
2020-04-16 21:12:44 -0700

    Adding buildpack

--- src/models/firebaseProperties.js
+++ src/models/firebaseProperties.js
@@ -3,22 +3,8 @@ const HOMEDIR = path.join(__dirname, "..", "..");
 const CONFIG = require(path.join(HOMEDIR, "config", "default"));
 const admin = require('firebase-admin');
 
-
-var serviceAccount = {
-    "type": "service_account",
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "project_id": process.env.FIREBASE_PROJECT_ID
-};
-
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
     databaseURL: CONFIG.FIREBASE.DATABASE_URL
 });
 const fbData = admin.firestore();
