447cf11da49b40cb6b77fb38abe797d678903841
Jagmoah Singh <singhjagmohan4734@gmail.com>
2020-05-23 15:51:23 -0700

    Google oauth2

--- config/default.js
+++ config/default.js
@@ -22,7 +22,7 @@ const CONFIG = {
         SMPTP: {
             HOST: process.env.SMTP_HOST,
             PORT: process.env.SMTP_PORT,
-            SECURE: false,
+            SECURE: true,
             CLIENT: {
                 SECRET:process.env.CLIENT_SECRET,
                 ID:process.env.CLIENT_ID,
