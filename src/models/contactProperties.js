4262f5f91fffd6f3d2764f85d840c477aa342fd6
Jagmoah Singh <singhjagmohan4734@gmail.com>
2020-05-23 18:21:00 -0700

    Google oauth2

--- src/models/contactProperties.js
+++ src/models/contactProperties.js
@@ -4,15 +4,15 @@ var nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const CONFIG = require(path.join(HOMEDIR, "config", "default"));
-// const oauth2Client = new OAuth2(
-//     CONFIG.CONTACT.SMPTP.CLIENT.ID, // ClientID
-//     CONFIG.CONTACT.SMPTP.CLIENT.SECRET, // Client Secret
-//     CONFIG.CONTACT.SMPTP.CLIENT.REDIRECT_URL // Redirect URL
-// );
-// oauth2Client.setCredentials({
-//   refresh_token: CONFIG.CONTACT.SMPTP.CLIENT.REFRESH_TOKEN
-// });
-// const accessToken = oauth2Client.getAccessToken()
+const oauth2Client = new OAuth2(
+    CONFIG.CONTACT.SMPTP.CLIENT.ID,
+    CONFIG.CONTACT.SMPTP.CLIENT.SECRET,
+    CONFIG.CONTACT.SMPTP.CLIENT.REDIRECT_URL
+);
+oauth2Client.setCredentials({
+  refresh_token: CONFIG.CONTACT.SMPTP.CLIENT.REFRESH_TOKEN
+});
+const accessToken = oauth2Client.getAccessToken()
 const smtpTransport = nodemailer.createTransport({
   host: CONFIG.CONTACT.SMPTP.HOST,
   port: CONFIG.CONTACT.SMPTP.PORT,
@@ -21,8 +21,9 @@ const smtpTransport = nodemailer.createTransport({
     type: "OAuth2",
     user: CONFIG.CONTACT.SMPTP.AUTH.USER,
     serviceClient: CONFIG.CONTACT.SMPTP.CLIENT.ID,
-    privateKey: CONFIG.CONTACT.SMPTP.CLIENT.SECRET,
-    accessToken: process.env.ACCESS_TOKEN
+    clientSecret: CONFIG.CONTACT.SMPTP.CLIENT.SECRET,
+    refreshToken: CONFIG.CONTACT.SMPTP.CLIENT.REFRESH_TOKEN,
+    accessToken: accessToken
   }
 });
 module.exports = smtpTransport;
